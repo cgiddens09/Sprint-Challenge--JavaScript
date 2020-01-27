eba138b02c8701144bf582c7b18d75d19eae782d
Christina Giddens <giddens.christina@gmail.com>
2020-01-26 21:58:12 -0500

    unfinished project

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,25 +7,30 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
-
+let consume = function(a,b,cb){
+  return cb(a,b);
+}
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+let add =(a,b) => a + b
 
+let multiply = (a, b) => a * b
+let greeting = (a, b) => `Hello ${a} ${b}, nice to meet you!`
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ Step 3: Check your work by un-commenting the following calls to consume(): 
+console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: When something is not defined within a function, it will look outside of its scope to find a reference to what is needed.
 
 
 const external = "I'm outside the function";
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,25 +6,26 @@
   object name, diet, weight, length, period
 */
 
-// tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+ const dino1 ={object_name: "tyrannosaurus", diet: "carnivorous", weight:"7000kg", lenght:"12m", period: "Late Cretaceous"};
 
-// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+ const dino2 ={object_name: "stegosaurus", diet: "herbivorous", weight: "2000kg", length: "9m", period: "Late Jurassic"};
 
-// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+ const dino3 ={object_name: "velociraptor", diet: "carnivorous", weight: "15kg", lenght: "1.8m", period: "Late Cretaceous"};
 
-// Using your dinosaur objects, log answers to these questions:
+
+ Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dino1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dino3.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dino2.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dino1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
@@ -51,8 +52,15 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+
+const universities =[];
+
+  for(let i = 0; i < graduates.length; i++){
+   universities.push(graduates[i].university);
+    universities.sort();
+  };
+  console.log(universities);
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -61,7 +69,13 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+
+  for (let i = 0; i < graduates.length; i++){
+    contactInfo.push(graduates[i].first_name, graduates[i].email);
+  };
+
 console.log(contactInfo);
+ 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
